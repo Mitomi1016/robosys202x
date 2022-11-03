4a40514e84db06c2fe9f4ca9a4c99ea78529169e
Shoya Mitomi <shoya4108@icloud.com>
2022-11-03 12:08:50 +0900

    test

--- test.bash
+++ test.bash
@@ -1,17 +1,26 @@
-#!/bin/bash -xv
-# SPDX-FileCopyrightText: 2022 Shoya Mitomi
-# SPDX-License-Identifier: BSD-3-Clause
+#!/bin/bash
+# SPDX-...
+# SPDX-...
 
 ng () {
-	echo NG at Line $1
-	res=1
-}
+		echo ${1}行目が違うよ
+			res=1
+		}
 
-res=0
+		res=0
 
-### I/O TEST ###
-out=$(seq 5 | ./plus)
-[ "${out}" = 14 ] || ng ${LINENO}
+		### I/O TEST ###
+		out=$(seq 5 | ./plus)
+		[ "${out}" = 15 ] || ng "${LINENO}"
 
-[ "$res" = 0 ] && echo OK        # &&（AND記号）は左側が成功すると右側を実行
-exit $res
+		### STRANGE INPUT ###
+		out=$(echo あ | ./plus)
+		[ "$?" = 1 ] || ng "${LINENO}"
+		[ "${out}" = "" ] || ng "${LINENO}"
+
+		out=$(echo | ./plus)
+		[ "$?" = 1 ] || ng "${LINENO}"
+		[ "${out}" = "" ] || ng "${LINENO}"
+
+		[ "${res}" = 0 ] && echo OK
+		exit $res
