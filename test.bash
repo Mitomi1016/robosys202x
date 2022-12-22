ed20caceb92101188b37939c91acb4b98ec2dc4a
Shoya Mitomi <s21C1119XH@s.chibakoudai.jp>
2022-12-23 05:20:38 +0900

    update plus

--- test.bash
+++ test.bash
@@ -3,16 +3,23 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-	echo NG at LINE $1	
+	echo NG at LINE $1
 	res=1
 }
 
 res=0
 
-##I/O TEST ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ]  || ng ${LINENO}
 
-[ "$res" = 0 ] && echo OK
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] 	  || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
 exit $res
